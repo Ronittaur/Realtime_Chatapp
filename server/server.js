04afa2f908dbb1521782252f7a3e73aa8b640d3d
Ronit Taur <ronittaur6@gmail.com>
2025-09-18 17:39:54 +0530

    another update for versel

--- server/server.js
+++ server/server.js
@@ -45,9 +45,11 @@ app.use("/api/status", (req, res)=>res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter)
 
-
-if(process.env.NODE_ENV==="production"){//Connect to MongoDB
+//Connect to MongoDB
 await connectDB();
+
+if(process.env.NODE_ENV!=="production"){
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,()=> console.log("Server is Running on PORT:"+ PORT));
 }
