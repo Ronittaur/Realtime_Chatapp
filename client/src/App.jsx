3911d80f58f630cd0706cdd49f63db35163bb3ca
Ronit Taur <ronittaur6@gmail.com>
2025-09-18 16:52:48 +0530

    firstcommit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Login from './pages/Login'
+import ProfilePage from './pages/ProfilePage'
+import assets from './assets/assets'
+import{Toaster} from "react-hot-toast"
+import { AuthContext } from '../context/AuthContext'
+import { ChatProvider } from '../context/ChatContext'
+
+const App = () => {
+  const{ authUser } = useContext(AuthContext)
+  return (
+    <div className="min-h-screen w-full bg-[url('./src/assets/bgImage.svg')]
+    bg-cover bg-no-repeat bg-center">
+      <Toaster/>
+      <Routes>
+        <Route path='/'element={authUser ? <HomePage/>: <Navigate to= "/login"/>}/>
+        <Route path='/login'element={!authUser ? <Login/>:<Navigate to="/"/>}/>
+        <Route path='/Profile'element={authUser ?<ProfilePage/>:<Navigate to= "/login"/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
